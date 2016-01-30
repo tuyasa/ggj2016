b0cbe68a1f5c9173c5454d0d6351e1354cf7748a
gogeric <gogeric@hotmail.fr>
2016-01-31 00:00:30 +0100

    HouseMoreDetailed

---
+++ ggj2016/Assets/ToiletSurprise.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToiletSurprise : MonoBehaviour {
+
+    SpriteRenderer sr;
+    ParticleSystem pe;
+
+
+    public void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        pe = GetComponent<ParticleSystem>();
+    }
+
+    [ContextMenu("FlipLid")]
+    public void FlipLid()
+    {
+        sr.flipY = !sr.flipY;
+        if (!sr.flipY) pe.Play();
+        else pe.Stop();
+    }
+}
