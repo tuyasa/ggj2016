3bbd8ba03b1680ae26c8242fff8b928d0cae632d
Sabouraud Raphael <tuyasa@MacBook-Pro-de-Sabouraud.local>
2016-01-31 01:04:54 +0100

    doors work

---
+++ ggj2016/Assets/openDoor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		DoorAnimator door = other.GetComponent<DoorAnimator>();
+		if(door == null)
+			return;
+		door.PlayAnim();
+		door.twin.PlayAnim();
+	}
+}
